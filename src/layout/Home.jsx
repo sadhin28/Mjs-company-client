f4ec14772fa7989d017b2d53e7746b74b36fdc19
sadhin28 <taosifsadhin113@gmail.com>
2025-06-26 05:30:37 +0600

    all done

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -1,13 +1,12 @@
 import { useLoaderData } from "react-router-dom";
+import Cards from "../components/Cards";
 
 const Home = () => {
     const bulbs = useLoaderData()
     return (
         <div>
            {
-             bulbs.map(data=><div>
-                <h1>{data.name}</h1>
-             </div>)
+             bulbs.map(data=><Cards data={data}></Cards>)
            }
         </div>
     );
