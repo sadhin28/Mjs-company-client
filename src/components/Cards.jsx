af4e7ca923c95954c17847f579b68fd4b278acb5
sadhin28 <taosifsadhin113@gmail.com>
2025-06-27 01:27:10 +0600

    add card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,33 +1,35 @@
-import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { FcDeleteDatabase } from "react-icons/fc";
 const Cards = ({data}) => {
   return (
-   
-      <Card className='border'>
+   <div className='w-70'>
+    
+      <Card>
       
-     <CardMedia>
+     <CardMedia className='flex justify-items-center'>
       <img src={data.photo} alt="" />
      </CardMedia>
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          {data.name}
         </Typography>
         <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+         {data.details}
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
+      <CardActions className='flex bg-amber-200 justify-between'>
+        <Button size="small">View</Button>
+        <Button size="small">Update</Button>
+        <Button className='bg-black' size="large"><FcDeleteDatabase /></Button>
       </CardActions>
     </Card>
      
+   </div>
   );
 };
 
