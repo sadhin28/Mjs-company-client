6a9908e61cd64bf8c8873826fbcd6a3f4c4554fe
sadhin28 <taosifsadhin113@gmail.com>
2025-06-29 20:16:04 +0600

    add bot led light

--- src/components/Errorpage.jsx
+++ src/components/Errorpage.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Errorpage = () => {
     return (
         <div>
-            <h1 className='text-6xl flex '>Error</h1>
+            <div className='mb-50'>
+                 <Link to="/" className="absolute top-4  left-4 text-lg font-semibold text-gray-700 hover:underline">
+                    ← Back to home
+                </Link>
+            </div>
+            <h1 className='text-6xl text-center'>Error</h1>
         </div>
     );
 };
