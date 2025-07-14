161e71398bce33db8c8643739af7c762c49fb482
sadhin28 <taosifsadhin113@gmail.com>
2025-07-14 20:45:50 +0600

    add all user

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,11 +2,17 @@
 import { useLoaderData } from "react-router-dom";
 
 const User = () => {
-    const user = useLoaderData()
+    const data = useLoaderData()
+    
     return (
-        <div>
-            <h1>{user.name}</h1>
+        <div className="grid grid-cols-2 gap-4 p-4">
+      {data.map((item) => (
+        <div key={item.id} className="border p-4 rounded shadow">
+          <h2 className="text-xl font-bold">{item.name}</h2>
+          <p className="text-gray-600">Email: {item.email}</p>
         </div>
+      ))}
+    </div>
     );
 };
 
