027083353b8991914b34bbb0c07314f69af10a8c
sadhin28 <taosifsadhin113@gmail.com>
2025-07-16 21:37:55 +0600

    add dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Dashboard = () => {
+    const items =useLoaderData()
     return (
         <div>
-            <h1>Dashboard</h1>
+           
+            <h1>{items.name}</h1>
         </div>
     );
 };
